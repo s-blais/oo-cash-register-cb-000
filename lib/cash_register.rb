24bd763424ae091c58a9f4585f9cfc919e708939
Stephen Blais <stephen@s-blais.com>
2019-08-29 19:21:26 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,28 @@
+class CashRegister
 
+  attr_accessor :cash_register :cash_register_with_discount :total :discount
+
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+  end
+
+  def total
+    @total
+  end
+
+  def add_item (title, price, quantity = 1)
+    @total += price*quantity
+  end
+
+  def apply_discount
+
+  end
+
+  def void_last_transaction
+
+  end
+
+
+
+end
