13ccdfe6ff788970b0d77c8f87d369e7c5493aca
Stephen Blais <stephen@s-blais.com>
2019-08-29 19:39:29 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -19,6 +19,7 @@ class CashRegister
 
     discount_amount = (@total*@discount)/100
     @total -= discount_amount
+    puts "After the discount, the total comes to #{@total}"
   end
 
   def void_last_transaction
