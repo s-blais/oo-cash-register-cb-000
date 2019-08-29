bffc523cc67277e0960a665469f37c0c4426d085
Stephen Blais <stephen@s-blais.com>
2019-08-29 19:30:39 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,7 +16,7 @@ class CashRegister
   end
 
   def apply_discount
-    @cash_register_with_discount -= @total*(@discount/100)
+    @cash_register_with_discount = @total - @total*(@discount/100)
   end
 
   def void_last_transaction
